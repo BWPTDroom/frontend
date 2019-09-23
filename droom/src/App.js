4755634f638969d3ff6f3994583cebd689b61b57
roselandroche <r.landroche@gmail.com>
2019-09-23 19:19:42 -0400

    Editing names etc.

--- droom/src/App.js
+++ droom/src/App.js
@@ -4,14 +4,14 @@ import CreateProfile from './components/CreateProfile';
 import SignUp from './components/SignUp';
 import CreateCompanyProfile from './components/CreateCompanyProfile';
 import CreateJobListing from './components/CreateJobListing';
-import HomePage from './components/HomePage';
+import Nav from './components/Nav';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/signup' component={SignUp} />
+      <Route path='/' component={Nav} />
+      <Route exact path='/' component={SignUp} />
       <Route path='/employeeprofile' component={CreateProfile} />
       <Route path='/companyprofile' component={CreateCompanyProfile} />
       <Route path='/newjob' component={CreateJobListing} />
