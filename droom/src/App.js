541fa75101e88cfb4011016a80fd8f894ee0bcd0
roselandroche <r.landroche@gmail.com>
2019-09-23 21:55:19 -0400

    Added dummy job listings

--- droom/src/App.js
+++ droom/src/App.js
@@ -11,7 +11,7 @@ import LogIn from './components/LogIn';
 
 function App() {
 
-  const [listings, setNewListing] = useState([{position: 'Full Stack Developer', req_skills: 'HTML, CSS, JS', bonus_skills: 'Teamwork'}]);
+  const [listings, setNewListing] = useState([{company: 'Apple', position: 'Full Stack Developer', req_skills: 'HTML, CSS, JS', bonus_skills: 'Teamwork'}, {company: 'Google', position: 'Backend Developer', req_skills: 'React, JS', bonus_skills: 'Innovative'}, {company: 'Amazon', position: 'Frontend Developer', req_skills: 'HTML, CSS', bonus_skills: 'Design'}]);
   // const [companies, setNewCompany] = useState([]);
   // const [users, setNewUser] = useState([]);
   const [members, setMembers] = useState();
