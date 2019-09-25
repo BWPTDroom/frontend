ee0b3e49e00b0692eac1d92ca7820078b95c59aa
Andrew <andrewryanogle@gmail.com>
2019-09-24 17:30:12 -0700

    refactored code

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import './App.css';
+import './css/index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import CreateProfile from './components/CreateProfile';
 import SignUp from './components/SignUp';
 import CreateCompanyProfile from './components/CreateCompanyProfile';
 // import CreateJobListing from './components/CreateJobListing';
-import Nav from './components/Nav';
 import JobCard from './components/JobCard';
 import LogIn from './components/LogIn';
 
@@ -20,7 +19,6 @@ function App() {
 
   return (
     <div className='App'>
-      <Route path='/' component={Nav} />
       <Route exact path='/' render={props => <SignUp {...props} members={members} setMembers={setMembers} />} />
       <Route path='/employeeprofile' component={CreateProfile} />
       <Route path='/companyprofile' component={CreateCompanyProfile} />
--- droom/src/components/LogIn.js
+++ droom/src/components/LogIn.js
@@ -5,75 +5,75 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 
-const StyledForm = styled(Form)`
-    background-image: linear-gradient(175deg, rgb(24,38,40, 0.8), rgb(24,38,40, 1));
-    width: 380px;
-    max-width: 100%;
-    height: auto;
-    margin: 18px auto;
-    padding: 30px;
-    box-sizing: border-box;
-    box-shadow: 5px 10px 15px rgb(95, 99, 102, 0.3);
-`
+// const StyledForm = styled(Form)`
+//     background-image: linear-gradient(175deg, rgb(24,38,40, 0.8), rgb(24,38,40, 1));
+//     width: 380px;
+//     max-width: 100%;
+//     height: auto;
+//     margin: 18px auto;
+//     padding: 30px;
+//     box-sizing: border-box;
+//     box-shadow: 5px 10px 15px rgb(95, 99, 102, 0.3);
+// `
 
-const Title = styled.h1`
-    font-size: 1.5rem;
-    margin-top: 100px;
-    color: rgb(242,242,242);
-    font-family: 'Maven Pro';
-    text-align: center;
-`
+// const Title = styled.h1`
+//     font-size: 1.5rem;
+//     margin-top: 100px;
+//     color: rgb(242,242,242);
+//     font-family: 'Maven Pro';
+//     text-align: center;
+// `
 
-const SubTitle = styled.h2`
-    font-size: 0.9rem;
-    margin-bottom: 50px;
-    color: rgb(101,204,184, 1);
-    font-weight: lighter;
-    font-family: 'Roboto';
-    text-align: center;
-`
+// const SubTitle = styled.h2`
+//     font-size: 0.9rem;
+//     margin-bottom: 50px;
+//     color: rgb(101,204,184, 1);
+//     font-weight: lighter;
+//     font-family: 'Roboto';
+//     text-align: center;
+// `
 
-const StyledField = styled(Field)`
-    display: block;
-    height: 50px;
-    width: 318px;
-    margin: 18px 0px;
-    border-radius: 2px;
-    border: 1px solid lightgrey;
-    padding: 15px;
-    box-sizing: border-box;
-    background-color: rgb(242,242,242);
-    font-family: 'Roboto';
-    ::placeholder {
-        color: rgb(24,38,40, 1);
-        font-family: 'Roboto';
-    }
-`
+// const StyledField = styled(Field)`
+//     display: block;
+//     height: 50px;
+//     width: 318px;
+//     margin: 18px 0px;
+//     border-radius: 2px;
+//     border: 1px solid lightgrey;
+//     padding: 15px;
+//     box-sizing: border-box;
+//     background-color: rgb(242,242,242);
+//     font-family: 'Roboto';
+//     ::placeholder {
+//         color: rgb(24,38,40, 1);
+//         font-family: 'Roboto';
+//     }
+// `
 
-const StyledOption = styled.option`
-    width: 318px;
-    margin: 18px 0px;
-    border-radius: 2px;
-    border: 1px solid lightgrey;
-    padding: 15px;
-    box-sizing: border-box;
-    background-color: rgb(242,242,242);
-    font-family: 'Roboto';
-    color: rgb(24,38,40, 1);
-`
+// const StyledOption = styled.option`
+//     width: 318px;
+//     margin: 18px 0px;
+//     border-radius: 2px;
+//     border: 1px solid lightgrey;
+//     padding: 15px;
+//     box-sizing: border-box;
+//     background-color: rgb(242,242,242);
+//     font-family: 'Roboto';
+//     color: rgb(24,38,40, 1);
+// `
 
-const StyledButton = styled.button`
-    height: 50px;
-    width: 318px;
-    margin: 30px 0;
-    border-radius: 0px;
-    background-image: linear-gradient(75deg, rgb(101,204,184, 0.6), rgb(101,204,184, 1));
-    border: 1px solid rgb(101,204,184, 1);
-    color: rgb(24,38,40, 1);
-    text-transform: uppercase;
-    font-family: 'Maven Pro';
-    font-size: 0.8rem;
-`
+// const StyledButton = styled.button`
+//     height: 50px;
+//     width: 318px;
+//     margin: 30px 0;
+//     border-radius: 0px;
+//     background-image: linear-gradient(75deg, rgb(101,204,184, 0.6), rgb(101,204,184, 1));
+//     border: 1px solid rgb(101,204,184, 1);
+//     color: rgb(24,38,40, 1);
+//     text-transform: uppercase;
+//     font-family: 'Maven Pro';
+//     font-size: 0.8rem;
+// `
 const StyledP = styled.p`
     color: rgb(101,204,184, 1);
     font-family: 'Roboto';
@@ -92,23 +92,23 @@ const LogIn = ({ errors, touched, status }) => {
 
        return (
        <>
-              <StyledForm className="logIn">
-                    <Title>Droom</Title>
-                    <SubTitle>Welcome Back!</SubTitle>
+              <div className="forms">
+                    <h1>Droom</h1>
+                    <h2>Welcome Back!</h2>
                      {touched.username && errors.username && <StyledP className="error">{errors.username}</StyledP>}
-                     <StyledField name="username" 
+                     <input name="username" 
                             type="text" 
                             placeholder="Username" 
                             /> 
                      {touched.password && errors.password && <StyledP className="error">{errors.password}</StyledP>}
-                     <StyledField name="password" 
+                     <input name="password" 
                             type="password" 
                             placeholder="Password" 
                             /> 
-                     <StyledButton type="login">
+                     <button type="login">
                             Log In
-                     </StyledButton>
-              </StyledForm>
+                     </button>
+              </div>
        </>
        )
 }
